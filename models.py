1fe016c62faee0f27265f0876c2a21cda78c1405
Vladislav <166797675+le-chien-glascheit@users.noreply.github.com>
2024-08-27 11:44:49 +0300

    исправления и доработки api
    В файле routes.py исправлены:
    - Оптимизирована функция "получить посты"
    - В функции "Изменить учётные данные" Добавлены наименования методам для пользователей и постов.
    - Функция "Получить пользователей" была переписана
    Более конкретные изменения по строчкам:
    * изменения функции на строках 52-56 (55-120)
    * название функции 152-158 (175-181) 
    * оптимизация запроса в get_post 169-182 (189-202)
    
    
    В файлах dependencies.py и models.py отформатированы строки.

---
+++ models.py
@@ -0,0 +1,218 @@
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy import UUID, ForeignKey, Uuid, create_engine
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    Session,
+    declared_attr,
+    mapped_column,
+    relationship,
+)
+
+from blogs.settings import DB_URL
+
+
+class Base(DeclarativeBase):
+    """
+    id: UUID
+    """
+
+    id: Mapped[UUID] = mapped_column(Uuid, primary_key=True, default=uuid4)
+
+    @declared_attr
+    @classmethod
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+
+class Post(Base):
+    """
+    title: str
+    text: str
+    """
+
+    title: Mapped[str]
+    text: Mapped[str]
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+
+    user: Mapped['User'] = relationship(back_populates='posts')
+
+    def __repr__(self):
+        return repr(f'{self.title} posted {self.user_id}')
+
+
+class Sub(Base):
+    """
+    follower_id: UUID
+    followed_id: UUID
+    sub_date: datetime = mapped_column(default=datetime.now)
+    """
+
+    follower_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+    followed_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
+    sub_date: Mapped[datetime] = mapped_column(default=datetime.now)
+
+    follower: Mapped['User'] = relationship(
+        foreign_keys='Sub.follower_id',
+        back_populates='following',
+    )
+    followed: Mapped['User'] = relationship(
+        foreign_keys='Sub.followed_id',
+        back_populates='followers',
+    )
+
+    def __repr__(self):
+        return repr(f'Sub id:{self.id}')
+
+
+class User(Base):
+    """
+    name: str
+    email: str
+    password: str
+    """
+
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+    posts: Mapped[list[Post]] = relationship(back_populates='user')
+    followers: Mapped[list[Sub]] = relationship(
+        foreign_keys='Sub.followed_id',
+        back_populates='followed',
+    )
+    following: Mapped[list[Sub]] = relationship(
+        foreign_keys='Sub.follower_id',
+        back_populates='follower',
+    )
+
+    def __repr__(self):
+        return repr(f'User(id={self.id}, name={self.name})')
+
+
+if __name__ == '__main__':
+    print('creating db')
+    engine = create_engine(DB_URL)
+    Base.metadata.create_all(engine)
+
+    user1 = User(name='vlad', email='vlad@mail.com', password='blabla')
+    user2 = User(name='pasha', email='pasha@mail.com', password='blabla')
+    user3 = User(name='alex', email='alex@mail.com', password='blabla')
+    user4 = User(name='liza', email='liza@mail.com', password='blabla')
+    user5 = User(name='avdotya', email='avdotya@mail.com', password='blabla')
+    user6 = User(name='senya', email='senya@mail.com', password='blabla')
+    user7 = User(
+        name='rhaenyra',
+        email='rhaenyra@mail.com',
+        password='blabla',
+    )
+
+    post1 = Post(
+        title='Винипух',
+        text='...в голове моей опилки...',
+        user=user1,
+    )
+
+    post2 = Post(
+        title='Выбил три леги в десятке',
+        text='Не буду их качать, так как они для коллекции',
+        user=user2,
+    )
+
+    post3 = Post(
+        title='Собираю всех персонажей',
+        text='осталось 5',
+        user=user2,
+    )
+
+    post4 = Post(
+        title='Лиза',
+        text='Интересно почему у Лизы нет ни постов'
+        'ни связей с другими пользователями',
+        user=user3,
+    )
+
+    post5 = Post(
+        title='Я есть Авдотья',
+        text='Я служу своей Барыне верой и правдой',
+        user=user5,
+    )
+
+    post6 = Post(
+        title='Вольная',
+        text='Мне она уже не к чему, тружусь ради дочки',
+        user=user5,
+    )
+
+    post7 = Post(
+        title='Самолетик',
+        text='Отец купил мне новую игрушку...))) Лада...',
+        user=user6,
+    )
+
+    post8 = Post(
+        title='Про меня',
+        text="""Рейенира Таргариен — персонаж вымышленного мира,
+         изображённого в серии книг «Песнь Льда и Огня» Джорджа Мартина,
+          королева Вестероса из валирийской династии Таргариенов,
+          которая боролась за престол со своим единокровным братом Эйегоном II
+           в ходе Пляски Драконов. Жена Дейемона Таргариена""",
+        user=user7,
+    )
+
+    post9 = Post(
+        title='Кем является Рейнира для дейнерис?',
+        text="""Старший брат Визериса и Дейнерис Таргариенов,
+         муж Элии Мартелл, в браке с которой родились двое
+         детей — Рейнис и Эйгон. Он также является отцом
+         Джона Сноу от Лианны Старк, на которой тайно женился
+         после аннулирования брака с Элией.""",
+        user=user7,
+    )
+
+    post10 = Post(
+        title='Что стало с детьми Рейниры?',
+        text="""Позже, в Битве при Глотке погиб старший сын и наследник
+         Рейниры Джекейрис, а самый младший сын пропал без вести.""",
+        user=user7,
+    )
+
+    post11 = Post(
+        title='Почему поменяли актрису Рейниры?',
+        text="""29-летнюю белокурую наследницу престола теперь грает Эмма
+        Д Арси, а супругу короля — Оливия Кук. На такое решение создатели
+        сериала пошли из-за скачка во времени: по словам сценаристов,
+        им важно было показать, что за 10 лет героини сильно изменились.""",
+        user=user7,
+    )
+
+    post12 = Post(
+        title='Кто станет королем после Рейниры?',
+        text="""Королём станет Эйгон |||, сын Рейниры от Деймона,
+        его женой будет Джейхейра Таргариен,
+        внучка Алисенты, дочь Эйгона ||""",
+        user=user7,
+    )
+
+    with Session(engine) as session:
+        for user in user1, user2, user3, user4, user5, user6, user7:
+            session.add(user)
+        for post in (
+            post1,
+            post2,
+            post3,
+            post4,
+            post5,
+            post6,
+            post7,
+            post8,
+            post9,
+            post10,
+            post11,
+            post12,
+        ):
+            session.add(post)
+
+        session.commit()
---
+++ routes.py
@@ -0,0 +1,269 @@
+import types
+import uuid
+from collections.abc import Sequence
+from enum import StrEnum
+from uuid import UUID
+
+from fastapi import APIRouter, status
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import joinedload
+
+from blogs.dependencies import CurrentUser, Session
+from blogs.exceptions import (
+    IncorrectDataError,
+    NotEnteredDataError,
+    UserNotFoundError,
+)
+from blogs.models import Post, Sub, User
+from blogs.schemas import PostIn, PostOut, UserIn, UserOut
+
+BLOG_PATH = '/user'
+POST_PATH = '/post'
+
+user_router = APIRouter(prefix=BLOG_PATH, tags=['user routes'])
+post_router = APIRouter(prefix=POST_PATH, tags=['post routes'])
+
+
+# # - [FastAPI Basic Auth](https://fastapi.tiangolo.com/advanced/security/http-basic-auth/)
+
+
+@user_router.post(
+    path='',
+    response_model_exclude_none=True,
+    name='Зарегистрироваться',
+    status_code=status.HTTP_201_CREATED,
+)
+def register_user(new_user: UserIn, session: Session) -> None:
+    """
+    Регистрация пользователей
+
+    Введите имя пользователя, свой адрес электронной почты и придумайте пароль.
+    """
+    session.add(
+        User(**new_user.model_dump()),
+    )
+    session.commit()
+
+
+class SearchMode(StrEnum):
+    FOLLOWERS = 'followers'
+    FOLLOWED = 'followed'
+    NONE = 'none'
+
+
+@user_router.get(
+    path='',
+    name='Получить пользователей',
+)
+def get_users(
+    session: Session,
+    subscribed_to_user: UUID | None = None,
+    followed: UUID | None = None,
+) -> list[UserOut]:
+    """
+    При выборе followers выводит список всех пользователей,
+    подписанных на автора, чей id вы указали.
+
+
+    При выборе followed выводит список пользователей,
+    на которых подписан указанный вами пользователь.
+
+    В случае если ничего не выбрать,
+    выводит всех пользователей текущей базы данных.
+    """
+
+    identifier = False
+
+    match type(subscribed_to_user), type(followed):
+        case types.NoneType, types.NoneType:
+            identifier = True
+            request = select(User)
+        case uuid.UUID, types.NoneType:
+            request = (
+                select(User)
+                .where(User.id == subscribed_to_user)
+                .options(joinedload(User.followers))
+            )
+        case types.NoneType, uuid.UUID:
+            request = (
+                select(User)
+                .where(User.id == followed)
+                .options(joinedload(User.following))
+            )
+        case _:
+            raise IncorrectDataError()
+
+    if identifier:
+        users = session.execute(request).unique().scalars().all()
+    else:
+        users1 = session.execute(request).unique().scalar_one()
+
+    target_users = []
+    match type(subscribed_to_user), type(followed):
+        case types.NoneType, types.NoneType:
+            target_users = [
+                UserOut.model_validate(user, from_attributes=True)
+                for user in users
+            ]
+        case uuid.UUID, types.NoneType:
+            target_users = [
+                UserOut.model_validate(sub.follower, from_attributes=True)
+                for sub in users1.followers
+            ]
+        case types.NoneType, uuid.UUID:
+            target_users = [
+                UserOut.model_validate(sub.followed, from_attributes=True)
+                for sub in users1.following
+            ]
+
+    return target_users
+
+
+@user_router.post(
+    path='/{user_id}/subscribe',
+    name='Подписаться на автора',
+    status_code=status.HTTP_201_CREATED,
+)
+def subscribe_to_user(
+    user_id: UUID,
+    subscribing_user: CurrentUser,
+    session: Session,
+) -> None:
+    """
+    Подписаться на выбранного пользователя по id.
+    """
+    try:
+        the_one_subscribe_to = session.execute(
+            select(User).where(User.id == user_id),
+        ).scalar_one()
+    except NoResultFound as err:
+        raise UserNotFoundError() from err
+
+    sub = Sub(
+        followed=the_one_subscribe_to,
+        follower=subscribing_user,
+    )
+    session.add(sub)
+
+    subscribing_user.following.append(sub)
+    session.add(the_one_subscribe_to)
+    session.commit()
+
+
+@post_router.post(
+    path='',
+    response_model_exclude_none=True,
+    name='Создать пост',
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(
+    post: PostIn,
+    user: CurrentUser,
+    session: Session,
+) -> None:
+    """
+    Создать пост:
+
+    Введите заголовок и текст.
+    """
+    new_post = Post(user=user, **post.model_dump())
+    session.add(new_post)
+    session.commit()
+
+
+def build_post_out(posts: Sequence[tuple[Post, str]]) -> list[PostOut]:
+    posts_out = []
+    for post, author in posts:
+        post_out = PostOut.model_validate(post, from_attributes=True)
+        post_out.author = author
+        posts_out.append(post_out)
+    return posts_out
+
+
+@post_router.get(
+    path='',
+    response_model_exclude_none=True,
+    name='Получить посты',
+)
+def get_posts(
+    session: Session,
+    user_id: UUID | None = None,
+) -> list[PostOut]:
+    """
+    Вывести все посты выбранного (по id) пользователя.
+    """
+    base_request = select(Post, User.name)
+    if user_id is None:
+        base_request = base_request.join(User)
+    else:
+        base_request = base_request.where(Post.user_id == user_id).join(User)
+    posts = session.execute(base_request).scalars().all()
+    return build_post_out(posts)
+
+
+@user_router.patch(
+    path='/{user_id}/',
+    response_model_exclude_none=True,
+    name='Изменить учётные данные',
+)
+def update_user(
+    author: CurrentUser,
+    session: Session,
+    name: str | None = None,
+    email: str | None = None,
+    password: str | None = None,
+) -> None:
+    """
+    Изменить данные вашей учётной записи
+
+    (имя пользователя, электронную почту, пароль)
+    """
+
+    if not any((name, email, password)):
+        raise NotEnteredDataError
+    else:
+        if name is not None:
+            author.name = name
+        if email is not None:
+            author.email = email
+        if password is not None:
+            author.password = password
+        session.add(author)
+        session.commit()
+
+
+@user_router.delete(
+    path='',
+    response_model_exclude_none=True,
+    name='Удалить учётную запись',
+)
+def delite_user(
+    user: CurrentUser,
+    session: Session,
+) -> None:
+    """
+    Удалить ваш аккаунт!!!
+    """
+    session.delete(user)
+    session.commit()
+
+
+@post_router.delete(
+    path='',
+    response_model_exclude_none=True,
+    name='Удалить пост',
+)
+def delite_post(
+    user: CurrentUser,
+    session: Session,
+    post_id: UUID,
+) -> None:
+    """
+    Удалить ваш выбранный пост по его id.
+    """
+    post = session.execute(
+        select(Post).where(Post.id == post_id).where(Post.user == user),
+    ).scalar_one()
+    session.delete(post)
+    session.commit()
